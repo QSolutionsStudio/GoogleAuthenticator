9ed745f785da4aa57f74e0f4a73368be5fdd5497
Wojciech Wnuk <wojtek@qsolutionsstudio.com>
2020-03-05 08:18:57 +0100

    Raw urlencode QR code generation fix

--- PHPGangsta/GoogleAuthenticator.php
+++ PHPGangsta/GoogleAuthenticator.php
@@ -104,9 +104,9 @@ class PHPGangsta_GoogleAuthenticator
         $height = !empty($params['height']) && (int) $params['height'] > 0 ? (int) $params['height'] : 200;
         $level = !empty($params['level']) && array_search($params['level'], array('L', 'M', 'Q', 'H')) !== false ? $params['level'] : 'M';
 
-        $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
+        $urlencoded = rawurlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
         if (isset($title)) {
-            $urlencoded .= urlencode('&issuer='.urlencode($title));
+            $urlencoded .= rawurlencode('&issuer='.urlencode($title));
         }
 
         return "https://api.qrserver.com/v1/create-qr-code/?data=$urlencoded&size=${width}x${height}&ecc=$level";
